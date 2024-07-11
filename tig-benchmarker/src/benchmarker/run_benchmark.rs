01a0d8dab3b1dff2182bfbfba4a9d22234911a39
FiveMovesAhead <ying@tig.foundation>
2024-07-11 23:31:40 +0800

    Add knapsack_optimised_knapmax feature to tig-benchmarker

--- tig-benchmarker/src/benchmarker/run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/run_benchmark.rs
@@ -6111,8 +6111,8 @@ pub async fn execute(
                                         // #[cfg(feature = "c003_a016")]
                                         // "c003_a016" => Some(tig_algorithms::knapsack::c003_a016::solve_challenge as SolveChallengeFn),
                                         
-                                        // #[cfg(feature = "c003_a017")]
-                                        // "c003_a017" => Some(tig_algorithms::knapsack::c003_a017::solve_challenge as SolveChallengeFn),
+                                        #[cfg(feature = "knapsack_optimised_knapmax")]
+                                        "c003_a017" => Some(tig_algorithms::knapsack::optimised_knapmax::solve_challenge as SolveChallengeFn),
                                         
                                         // #[cfg(feature = "c003_a018")]
                                         // "c003_a018" => Some(tig_algorithms::knapsack::c003_a018::solve_challenge as SolveChallengeFn),
