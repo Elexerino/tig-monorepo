49745b50dbc00f5fa5a40c0ad4971bff16ab5476
0xf216c29105217b34813718f8e62d57ef010200b0 <contact@tig.foundation>
2024-08-03 16:53:29 +0000

    "Player 0xf216c29105217b34813718f8e62d57ef010200b0 submitted 'inbound' for challenge vehicle_routing"

--- tig-benchmarker/src/benchmarker/run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/run_benchmark.rs
@@ -3143,8 +3143,8 @@ pub async fn execute(
                                         // #[cfg(feature = "c002_a031")]
                                         // "c002_a031" => Some(tig_algorithms::vehicle_routing::c002_a031::solve_challenge as SolveChallengeFn),
                                         
-                                        // #[cfg(feature = "c002_a032")]
-                                        // "c002_a032" => Some(tig_algorithms::vehicle_routing::c002_a032::solve_challenge as SolveChallengeFn),
+                                        #[cfg(feature = "vehicle_routing_inbound")]
+                                        "c002_a032" => Some(tig_algorithms::vehicle_routing::inbound::solve_challenge as SolveChallengeFn),
                                         
                                         // #[cfg(feature = "c002_a033")]
                                         // "c002_a033" => Some(tig_algorithms::vehicle_routing::c002_a033::solve_challenge as SolveChallengeFn),
