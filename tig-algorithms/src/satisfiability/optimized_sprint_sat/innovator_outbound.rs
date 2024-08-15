11fe326ac083b0791ac911dbc0bc347e5bd05d47
FiveMovesAhead <ying@tig.foundation>
2024-08-15 12:28:01 +0800

    Recompiled WASM for algorithm 'satisfiability/optimized_sprint_sat'

--- tig-algorithms/src/satisfiability/optimized_sprint_sat/innovator_outbound.rs
+++ tig-algorithms/src/satisfiability/optimized_sprint_sat/innovator_outbound.rs
@@ -17,7 +17,7 @@ use std::collections::{HashMap, HashSet};
 use tig_challenges::satisfiability::*;
 
 pub fn solve_challenge(challenge: &Challenge) -> anyhow::Result<Option<Solution>> {
-    let mut rng = StdRng::seed_from_u64(challenge.seed as u64);
+    let mut rng = StdRng::seed_from_u64(challenge.seeds[0] as u64);
     let num_variables = challenge.difficulty.num_variables;
     
     // Pre-allocate vectors to avoid reallocations
